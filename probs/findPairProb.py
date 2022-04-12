4972964cb495280f9f27a2e33e5e2f685494cecd
aajmorgan <92998489+aajmorgan@users.noreply.github.com>
2022-04-12 14:21:47 -0400

    Update findPairProb.py

--- probs/findPairProb.py
+++ probs/findPairProb.py
@@ -1,15 +1,27 @@
-import deck_of_cards
+import math
 
+RIVERMAX = 7
+DECKLENGTH = 52
 
-def findProb(cards, ranks):
+def findProb(nums, ranks):
     if "pair" in ranks:
         return 1
-    if len(cards) == 6:
-        prob = 6 / 46
-        return prob
     else:
-        return 0
-    # elif len(cards) == 5:
-    #    probCurrentGetsPair =
-    #   probGetPairFromLastTwo =
-    #  prob = probCurrentGetsPair + probGetPairFromLastTwo
+        return calculate(nums)
+
+def calculate(cards):
+    cards_to_be_flipped = RIVERMAX - len(cards)
+    l = math.comb(RIVERMAX - cards_to_be_flipped, 1)
+    adding = 0
+    for i in range(cards_to_be_flipped):
+        adding += 3/(DECKLENGTH - len(cards) - i)
+    return l * adding
+
+'''
+testing below
+
+nums = [1, 2, 3, 4, 5]
+ranks = ["p"]
+print(findProb(nums, ranks))
+
+'''
