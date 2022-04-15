07d01dc12b6881e00901a3f84c7bb48b99c7d59e
aajmorgan <92998489+aajmorgan@users.noreply.github.com>
2022-04-15 16:54:43 -0400

    whoops again

--- probs/findFlushProb.py
+++ probs/findFlushProb.py
@@ -11,13 +11,16 @@ def findProb(suits, suitSet, ranks):
         total = 0
         for s in suitSet:
             c = suits.count(s)
-            t1 = 1 if cards_to_be_flipped == 0 else (MAXSUIT - c - 1) / (DECKLENGTH - len(suits) - 1)
-            t2 = 0 if cards_to_be_flipped == 0 else (MAXSUIT - c) / (DECKLENGTH - len(suits) - 1)
+            t1 = 1 if cards_to_be_flipped == 1 else (MAXSUIT - c - 1) / (DECKLENGTH - len(suits) - 1)
+            t2 = 0 if cards_to_be_flipped == 1 else (MAXSUIT - c) / (DECKLENGTH - len(suits) - 1)
+            t2 *= (DECKLENGTH - MAXSUIT - (len(suits) - c)) / (DECKLENGTH - len(suits))
+            print('t2:', t2)
             if c == FLUSH - cards_to_be_flipped:
                 total = (MAXSUIT - c) / (DECKLENGTH - len(suits)) * t1
                 break
             elif c > FLUSH - cards_to_be_flipped:
-                total = (MAXSUIT - c) / (DECKLENGTH - len(suits)) + t2
+                total = (MAXSUIT - c) / (DECKLENGTH - len(suits)) * ((DECKLENGTH - MAXSUIT - (len(suits) - c)) / (DECKLENGTH - len(suits) - 1))
+                total += t2
                 break
         return total
 
