7d2ee00e1e93fd9c750df66567224a37bcd20813
kristenchung <98041293+kristenchung@users.noreply.github.com>
2022-04-25 14:51:57 -0400

    Update findStraightProb.py

--- probs/findStraightProb.py
+++ probs/findStraightProb.py
@@ -8,6 +8,8 @@ def findProb(cards, ranks):
         prob = 0
         sorted_cards = sorted(cards)
         if len(cards) == 5:
+            denom = DECKLENGTH - len(sorted_cards)
+            poss_cards = 0
             if "fourKind" in ranks:
                 return 0
             if "threeKind" in ranks:
@@ -15,22 +17,26 @@ def findProb(cards, ranks):
             #5 in a row already
             if (sorted_cards[4] - sorted_cards[0] + 1) == 5:
                 prob = 1
-            #4 in a row already
+            #4 in a row already, can be in 2 places 
             if (sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4:
-                prob = SUITS/(DECKLENGTH - len(sorted_cards))
-            #3 in a row already
+                poss_cards = 2
+                prob = (poss_cards*SUITS)/(denom-1)
+            #3 in a row already, can be in 2 places
             if (sorted_cards[2] - sorted_cards[0] + 1) == 3 or (sorted_cards[4] - sorted_cards[2] + 1) == 3 or (sorted_cards[3] - sorted_cards[1] + 1) == 3:
-                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
-            #2 in a row already with 1 card one num away
+                poss_cards = 2
+                prob = (poss_cards*SUITS)/denom * ((poss_cards-1)*SUITS)/(denom-1)
+            #2 in a row already with 1 card one num away, can be in 3 places
             if "twoKind" in ranks and (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[2] - sorted_cards[1] == 2 or sorted_cards[3] - sorted_cards[2] == 2 or sorted_cards[4] - sorted_cards[3] == 2):
-                prob = (3*SUITS)/(DECKLENGTH - len(sorted_cards))
+                poss_cards = 3
+                prob = (poss_cards*SUITS)/denom
             #if its 4 consecutive even or odd numbers - then the next 2 could fill it in
             if (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[3] - sorted_cards[2] == 2) or (sorted_cards[4] - sorted_cards[3] == 2 or sorted_cards[3] - sorted_cards[2] == 2) or (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[4] - sorted_cards[3] == 2):
-                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
-                break
-            #special cases for 10 and 2 bc of the ace
+                poss_cards = 2 
+                prob = (poss_cards*SUITS)/(denom-1)
+            #special cases for 10 and 2 bc of the ace, can only be in 1 place
             if (sorted_cards[0] == 10 or sorted_cars[0] == 2) and ((sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4):
-                prob = SUITS/(DECKLENGTH - len(sorted_cards))
+                poss_cards = 1
+                prob = (SUITS*poss_cards)/denom
         #if 6 cards 
         else:
             if "fourKind" in ranks:
@@ -40,16 +46,15 @@ def findProb(cards, ranks):
             #5 in a row already
             if (sorted_cards[4] - sorted_cards[0] + 1) == 5 or (sorted_cards[5] - sorted_cards[1] + 1) == 5:
                 prob = 1
-            #4 in a row already
+            #4 in a row already, can be in 2 places 
             if (sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4 or (sorted_cards[5] - sorted_cards[2] + 1) == 4:
-                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
-            #3 in a row already
-            if (sorted_cards[2] - sorted_cards[0] + 1) == 3 or (sorted_cards[4] - sorted_cards[2] + 1) == 3 or (sorted_cards[3] - sorted_cards[1] + 1) == 3 or (sorted_cards[5] - sorted_cards[3] + 1) == 3:
-                prob = SUITS/(DECKLENGTH - len(sorted_cards))
-            #2 in a row already with 1 card one num away
-            if "twoKind" in ranks and (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[2] - sorted_cards[1] == 2 or sorted_cards[3] - sorted_cards[2] == 2 or sorted_cards[4] - sorted_cards[3] == 2 or sorted_cards[5] - sorted_cards[4] == 2):
-                prob = SUITS/(DECKLENGTH - len(sorted_cards))
+                poss_cards = 2 
+                prob = (poss_cards*SUITS)/(denom-1)
             #special cases for 10 and 2 bc of the ace
             if (sorted_cards[0] == 10 or sorted_cars[0] == 2) and ((sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4):
-                prob = SUITS/(DECKLENGTH - len(sorted_cards))
+                poss_cards = 1
+                prob = (SUITS*poss_cards)/denom
+            #if 3 in a row or less, no chance of getting a straight 
+            else:
+                prob = 0   
         return prob
