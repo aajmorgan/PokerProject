d8d39a5c2c2bc99ce3b77a477f3519a0d7b29b92
kristenchung <98041293+kristenchung@users.noreply.github.com>
2022-04-25 13:17:03 -0400

    Update findStraightProb.py

--- probs/findStraightProb.py
+++ probs/findStraightProb.py
@@ -1,6 +1,7 @@
 import deck_of_cards
-
-def findProb(cards, cardSet, ranks):
+DECKLENGTH = 52
+SUITS = 4
+def findProb(cards, ranks):
     if "straight" in ranks:
         return 1
     else:
@@ -16,20 +17,20 @@ def findProb(cards, cardSet, ranks):
                 prob = 1
             #4 in a row already
             if (sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4:
-                prob = 4/47
+                prob = SUITS/(DECKLENGTH - len(sorted_cards))
             #3 in a row already
             if (sorted_cards[2] - sorted_cards[0] + 1) == 3 or (sorted_cards[4] - sorted_cards[2] + 1) == 3 or (sorted_cards[3] - sorted_cards[1] + 1) == 3:
-                prob = 8/47
+                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
             #2 in a row already with 1 card one num away
             if "twoKind" in ranks and (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[2] - sorted_cards[1] == 2 or sorted_cards[3] - sorted_cards[2] == 2 or sorted_cards[4] - sorted_cards[3] == 2):
-                prob = 12/47
+                prob = (3*SUITS)/(DECKLENGTH - len(sorted_cards))
             #if its 4 consecutive even or odd numbers - then the next 2 could fill it in
             if (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[3] - sorted_cards[2] == 2) or (sorted_cards[4] - sorted_cards[3] == 2 or sorted_cards[3] - sorted_cards[2] == 2) or (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[4] - sorted_cards[3] == 2):
-                prob = 8/46
+                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
                 break
             #special cases for 10 and 2 bc of the ace
             if (sorted_cards[0] == 10 or sorted_cars[0] == 2) and ((sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4):
-                prob = 4/47
+                prob = SUITS/(DECKLENGTH - len(sorted_cards))
         #if 6 cards 
         else:
             if "fourKind" in ranks:
@@ -41,14 +42,14 @@ def findProb(cards, cardSet, ranks):
                 prob = 1
             #4 in a row already
             if (sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4 or (sorted_cards[5] - sorted_cards[2] + 1) == 4:
-                prob = 8/46
+                prob = (2*SUITS)/(DECKLENGTH - len(sorted_cards))
             #3 in a row already
             if (sorted_cards[2] - sorted_cards[0] + 1) == 3 or (sorted_cards[4] - sorted_cards[2] + 1) == 3 or (sorted_cards[3] - sorted_cards[1] + 1) == 3 or (sorted_cards[5] - sorted_cards[3] + 1) == 3:
-                prob = 4/46
+                prob = SUITS/(DECKLENGTH - len(sorted_cards))
             #2 in a row already with 1 card one num away
             if "twoKind" in ranks and (sorted_cards[1] - sorted_cards[0] == 2 or sorted_cards[2] - sorted_cards[1] == 2 or sorted_cards[3] - sorted_cards[2] == 2 or sorted_cards[4] - sorted_cards[3] == 2 or sorted_cards[5] - sorted_cards[4] == 2):
-                prob = 4/46
+                prob = SUITS/(DECKLENGTH - len(sorted_cards))
             #special cases for 10 and 2 bc of the ace
             if (sorted_cards[0] == 10 or sorted_cars[0] == 2) and ((sorted_cards[3] - sorted_cards[0] + 1) == 4 or (sorted_cards[4] - sorted_cards[1] + 1) == 4):
-                prob = 4/46
+                prob = SUITS/(DECKLENGTH - len(sorted_cards))
         return prob
