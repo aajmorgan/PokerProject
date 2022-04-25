735be845885c2d3cae25c7ca4cbfac88024437b6
aajmorgan <92998489+aajmorgan@users.noreply.github.com>
2022-04-25 00:25:36 -0400

    fixed

--- probs/findPairProb.py
+++ probs/findPairProb.py
@@ -1,8 +1,6 @@
-import math
-
 RIVERMAX = 7
 DECKLENGTH = 52
-NUMS = 4 # 1 for each suit
+NUMS = 4 # 1 card for each suit
 
 def findProb(nums, ranks):
     if "pair" in ranks:
@@ -12,15 +10,12 @@ def findProb(nums, ranks):
 
 def calculate(cards):
     cards_to_be_flipped = RIVERMAX - len(cards)
-    l = math.comb(len(cards), 1) # each card has same probability, so only have to do calculation once
     denom = DECKLENGTH - len(cards)
     if cards_to_be_flipped == 2:
-        total = ((NUMS - 1)/denom) * (denom - (NUMS - 1) - 1)/(denom - 1) + (denom - 3) / (denom) * ((NUMS - 1)/(denom - 1))
+        total = ((len(cards) * 3) / denom) + (denom - (len(cards) * 3)) / (denom) * (((len(cards) + 1) * 3)/(denom - 1))
     else:
-        total = (NUMS - 1)/denom
-    adding = 0 if cards_to_be_flipped == 1 else (NUMS / denom) * ((NUMS - 1) / (denom - 1))
-    total += adding
-    return l * total
+        total = (len(cards) * 3) / denom
+    return total
 
 '''
 testing below
