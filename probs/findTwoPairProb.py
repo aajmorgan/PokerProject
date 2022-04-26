ddb95f3a59ec9230480ceb9057014ad84712c6ad
Michael Lebreck <102753626+mlebreck@users.noreply.github.com>
2022-04-26 01:19:15 -0400

    Update findTwoPairProb.py

--- probs/findTwoPairProb.py
+++ probs/findTwoPairProb.py
@@ -1,8 +1,23 @@
 import deck_of_cards
 
-
 def findProb(nums, numSet, ranks):
+
     if "twoPair" in ranks:
         return 1
+    elif "pair" in ranks:
+        prob = calculate(numSet, nums)
     else:
-        return -1  # TODO: calculate this
+        if len(nums) == 5:
+            prob = (len(nums)* 3 / 47) * ((len(nums) - 1) * 3) / 46
+        else:
+            prob = 0
+    return prob
+
+def calculate(numSet, nums):
+    u = len(numSet) - 1
+    prob = (3 * u / 46)
+    if len(nums) == 5:   
+        prob = (3 * u/47) + ((47-(3 * u)) / 47) * (prob + 3/46)
+    return prob
+
+                
