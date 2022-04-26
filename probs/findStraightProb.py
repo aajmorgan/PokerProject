4cf377e4b4c48bb02d1c1bc2443f605f5105bb30
kristenchung <98041293+kristenchung@users.noreply.github.com>
2022-04-26 12:32:33 -0400

    Update findStraightProb.py

--- probs/findStraightProb.py
+++ probs/findStraightProb.py
@@ -6,7 +6,8 @@ def findProb(cards, ranks):
         return 1
     else:
         prob = 0
-        sorted_cards = set(sorted(cards))
+        no_dups_sorted_cards = set(sorted(cards))
+        sorted_cards = list(no_dups_sorted_cards)
         if len(cards) == 5:
             denom = DECKLENGTH - len(sorted_cards)
             poss_cards = 0
