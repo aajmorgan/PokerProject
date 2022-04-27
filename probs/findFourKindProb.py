84e124c09ddb39fb52eecdc1c4cd4c00733fce24
Michael Browne <70501685+michaelbrowne8@users.noreply.github.com>
2022-04-27 19:13:58 -0400

    Made based on equations

--- probs/findFourKindProb.py
+++ probs/findFourKindProb.py
@@ -4,32 +4,24 @@ def findProb(cards, cardSet, ranks):
     else:
         if "pair" not in ranks:
             return 0
-        diffRanks = len(cardSet)
+        needTwo = 2/47 + 1/46
+        needOne = 1/47 + 46/47 * 1/46
         if len(cards) == 5:
-            if "threeKind" not in ranks:
-                # Ex 22 3 4 5, or 22 33 4
-                if diffRanks == 4:
-                    prob = 2 / 47 * 1 / 46
-                elif diffRanks == 3:
-                    prob = 4 / 47 * 1 / 46
-                else:
-                    return -1  # should not be able to get
-                return prob
+            if "fullHouse" in ranks:
+                prob = needOne + needTwo
+            elif "threeKind" in ranks:
+                prob = needOne
+            elif "twoPair" in ranks:
+                prob = needTwo * 2
+            elif "pair" in ranks:
+                prob = needTwo
             else:
-                # Ex 222 33 or 222 3 4
-                if diffRanks == 3:
-                    prob = 46 / 47 * 1 / 46 + 1 / 47 * 46 / 46  # 46/46 because can be any card, just show work for now
-                elif diffRanks == 2:
-                    # add prob of the pair ending as four of a kind
-                    prob = 1 / 47 * 46 / 46 + 2 / 47 * 2 / 46 + 44 / 7 * 1 / 46
-                else:
-                    return -1  # should not be able to get
-                return prob
+                return -1  # Should not be able to return this
         else:  # 6 cards
             if "threeKind" not in ranks:
                 return 0
             prob = 1 / 46
-            if diffRanks == 2:
+            if len(cardSet) == 2:
                 # This would be for example, your cards are 333 222, can get a 3 or 2
                 prob *= 2
             return prob
