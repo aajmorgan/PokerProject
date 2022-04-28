03118f3c4fa3d62cb037a9063114112477a271da
kristenchung <98041293+kristenchung@users.noreply.github.com>
2022-04-28 12:02:22 -0400

    Update findStraightProb.py

--- probs/findStraightProb.py
+++ probs/findStraightProb.py
@@ -4,7 +4,9 @@ def findProb(cards, ranks, straightFlush=0):
     SUITS = 4 if straightFlush == 0 else 1
     if "straight" in ranks:
         return 1
-    elif "fourKind" or "fullHouse" in ranks:
+    elif "fourKind" in ranks:
+        return 0
+    elif "fullHouse" in ranks:
         return 0
     else:
         prob = 0
@@ -20,15 +22,17 @@ def findProb(cards, ranks, straightFlush=0):
         denom = DECKLENGTH - num_cards
         formula = ((poss_cards*SUITS)/denom)*(((poss_cards -1)*SUITS)/(denom-1))
         otherform = ((poss_cards_b*SUITS)/denom)*((poss_cards_b*(SUITS-1))*(denom-1))
+        index = 0
+        cardseq = 0
+        everyother = 0
+        length = len(sorted_cards)
         for card in sorted_cards:
-            index = 0
-            cardseq = 0
-            everyother = 0
-            curcard = card
+            if index+1 > length-1:
+                break
             nextcard = sorted_cards[index+1]
-            if nextcard == curcard -1:
+            if nextcard == card -1:
                 cardseq += 1
-            if nextcard == curcard-2:
+            if nextcard == card-2:
                 everyother += 1
             index += 1
         if num_cards == 5:
@@ -65,5 +69,4 @@ def findProb(cards, ranks, straightFlush=0):
             if cardseq == 3 and everyother == 1:
                 poss_cards_b = 1
                 prob = otherform
-        return prob
-
+    return prob
