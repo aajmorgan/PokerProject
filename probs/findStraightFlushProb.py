18765746837ffc4bdd3fc6eaf011e8fc227a3642
aajmorgan <92998489+aajmorgan@users.noreply.github.com>
2022-05-02 10:24:25 -0400

    done

--- probs/findStraightFlushProb.py
+++ probs/findStraightFlushProb.py
@@ -13,11 +13,12 @@ def findProb(cards, suits, suitSet, ranks):
         suitNum = 3 if len(cards) == 5 else 4
         newCards = []
         for suit in suitSet:
-            if suits.count(suits) >= suitNum:
+            if suits.count(suit) >= suitNum:
                 x = suit
-        if x != None:
+                break
+        if x is not None:
             for card in cards: #this is the actual cards, not numbers
                 if card.suit == x:
                     newCards.append(card.rank)
-            straight.findProb(newcards, set(newCards), ranks)
+            return straight.findProb(newCards, ranks, straightFlush=len(cards))
         return 0 #none of the suits had enough in the hand
